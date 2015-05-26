58fde73a476d45fe53333f6524011c6bb180494f
Fernando Daciuk <f.daciuk@gmail.com>
2015-05-26 18:53:00 -0300

    Remove unused code

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Node Rest
 
-## Usage
+## Installation
 
 ```sh
 npm install
 ```
+
+## Usage
+
+```sh
+node index.js
+```
+
+And it is ready on `http://localhost:3000` :)
+
+Enjoy!
--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 'use strict';
 
 var express = require( 'express' );
-var session = require( 'express-session' );
-
 var router = express.Router();
 var app = express();
 var port = process.env.PORT || 3000;
 
 require( './app/routes' )( router );
-
-app.use( session({
-    secret: '$3cr3tK&y',
-    resave: true,
-    saveUninitialized: true
-}));
-
 app.use( '/', router );
 
 app.listen( port );
